7b4f21d8cf2d3a45a4bfd8e96ef8d804855cf64e
lab273 <838523724@qq.com>
2024-11-11 20:23:28 +0800

    二分查找

--- binary_search/binary_search.c
+++ binary_search/binary_search.c
@@ -1,6 +1,30 @@
 #include "stdio.h"
 
+int func(int arr[], int target, int len) {
+	int i = 0, j = len - 1, m = (i + j) / 2;
+	while (i < j)
+	{
+		if (arr[m] == target)
+		{
+			return 1;
+		}
+		if (arr[m] > target)
+		{
+			// 1(i)  3  6(m)  9  11(j)
+			j = m - 1;
+		}
+		if (arr[m]<target)
+		{
+			i = m + 1;
+		}
+		m = (i + j) / 2;
+	}
+	return -1;
+}
 int main() {
 
-	printf("hello");
+	int arr[] = { 1,3,6,9,10,12 };
+	//int j = func(arr, 11, sizeof (arr) / sizeof (arr[0]));
+	int j = func(arr, 10, sizeof (arr) / sizeof (arr[0]));
+	printf("%d", j);
 }
